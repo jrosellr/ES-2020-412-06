64323a76fc8086b0588451521547d4a352c3c3f9
Arnau Closa <arnauclosam@gmail.com>
2020-05-13 20:08:50 +0200

    Creació variables Hotels, Cars.

--- Cars.py
+++ Cars.py
@@ -1,4 +1,7 @@
 class Cars:
 
-    def __init__(self):
-        pass
+    def __init__(self, code, brand, pick_up_place, days_reserved):
+        self.code = code
+        self.brand = brand
+        self.pick_up_place = pick_up_place
+        self.days_reserved = days_reserved
--- Hotels.py
+++ Hotels.py
@@ -1,4 +1,8 @@
 class Hotels:
 
-    def __init__(self):
-        pass
+    def __init__(self, code, name, num_clients, num_rooms, days_reserved):
+        self.code = code
+        self.name = name
+        self.num_clients = num_clients
+        self.num_rooms = num_rooms
+        self.days_reserved = days_reserved
