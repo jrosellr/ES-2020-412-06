ea6baf1c9ed93906d4a6afeee93776f8fdd73350
Arnau Closa <arnauclosam@gmail.com>
2020-05-13 20:19:18 +0200

    Creació variables Vols.

--- Flights.py
+++ Flights.py
@@ -1,4 +1,7 @@
 class Flights:
 
-    def __init__(self):
+    def __init__(self, code, destination, num_clients):
+        self.code = code
+        self.destination = destination
+        self.num_clients = num_clients
         pass
