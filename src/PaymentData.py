db5408bed53144a4be68f0f8c741ff8692c8e9af
joan.rosellr <joan.rosellr@gmail.com>
2020-05-14 13:44:47 +0200

    add payment data documentation and unit tests

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -1,10 +1,16 @@
-class PaymentData:  # TODO: Implement DTO internals
+class PaymentData:
     """ A Value-Object used to hold the payment data
 
+    :var user_name: The client's name
+    :var card_number: The card identifier number
+    :var security-code: Three-number security code
+    :var amount: The amount to be charged
+    :var credit_card_type: The card can be either VISA or MASTERCARD
     """
-    def __init__(self, name: str, card_code: str, security_code, amount, credit_card_type = None):
-        self.name = name
-        self.card_code = card_code
+
+    def __init__(self, user_name: str, card_number: str, security_code: str, amount: float):
+        self.user_name = user_name
+        self.card_number = card_number
         self.security_code = security_code
         self.amount = amount
-        self.credit_card_type = credit_card_type
+        self.credit_card_type = None  # To be added in the future
