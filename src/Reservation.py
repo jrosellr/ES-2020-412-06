01b31b6cd941bbdf55ec278f2e04037d643397ac
Arnau Closa <arnauclosam@gmail.com>
2020-05-14 14:08:52 +0200

    Add Class Travel & Reservation.

---
+++ src/Reservation.py
@@ -0,0 +1,18 @@
+class Reservation:
+
+    def __init__(self, trabel: Trabel, user: User, payment_data: PaymentData):
+        self.trabel = trabel
+        self.user = user
+        self.payment_data = payment_data
+
+    def calculate_flights_price(self, price):
+        return (price * len(travel.flights.flights) * travel.flights.flights[0].num_clients)
+
+    def calculate_hotels_price(self, price):
+        pass
+
+    def calculate_cars_price(self, price):
+        pass
+
+    def calculate_total_price(self, flights_price, hotels_price, cars_price):
+        self.total_price = self.calculate_fligts_price(flights_price) + self.calculate_hotels_price(hotels_price) + self.calculate_cars_price(cars_price)
