e0f1a6f86ad1be895f4414941229d19b64e2f6a3
Arnau Closa <arnauclosam@gmail.com>
2020-05-14 12:14:13 +0200

    Creation __init__ of class PaymentData.

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -2,5 +2,9 @@ class PaymentData:  # TODO: Implement DTO internals
     """ A Value-Object used to hold the payment data
 
     """
-    def __init__(self):
-        pass
+    def __init__(self, name: str, card_code: str, security_code, amount, credit_card_type = None):
+        self.name = name
+        self.card_code = card_code
+        self.security_code = security_code
+        self.amount = amount
+        self.credit_card_type = credit_card_type
