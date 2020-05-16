51aca24e8678fdaa7a04c697fc65cda0f64591ed
joan.rosellr <joan.rosellr@gmail.com>
2020-05-16 20:39:23 +0200

    extend travel unit tests

--- tests/test_travel.py
+++ tests/test_travel.py
@@ -12,6 +12,36 @@ def test_travel_ctor():
     assert len(travel.flights.flights) != 0
 
 
+def test_travel_add_flight_empty_flights():
+    new_flight = Flight('00', '', 0)
+    flights = Flights([])
+    travel = Travel(flights)
+    travel.add_flight(new_flight)
+    assert len(travel.flights.flights) != 0
+    assert len(travel.flights.flights) == 1
+
+
+def test_add_flight_same_flight():
+    new_flight = Flight('00', '', 0)
+    flights = Flights([Flight('00', '', 0)])
+    travel = Travel(flights)
+    old_len = len(travel.flights.flights)
+    travel.add_flight(new_flight)
+    assert old_len != 0
+    assert len(travel.flights.flights) == old_len
+
+
+def test_add_flight_new_flight():
+    new_flight = Flight('01', '', 0)
+    flights = Flights([Flight('00', '', 0)])
+    travel = Travel(flights)
+    old_len = len(travel.flights.flights)
+    travel.add_flight(new_flight)
+    assert old_len != 0
+    assert len(travel.flights.flights) != old_len
+    assert len(travel.flights.flights) == old_len + 1
+
+
 def test_travel_get_num_clients_no_flights():
     flights = Flights([])
     travel = Travel(flights)
@@ -34,3 +64,13 @@ def test_travel_get_num_clients():
     ])
     travel = Travel(flights)
     assert travel.get_num_clients() == 8
+
+
+def test_travel_get_num_clients_add_flight():
+    flights = Flights([
+        Flight('00', '', 4),
+        Flight('01', '', 4)
+    ])
+    travel = Travel(flights)
+    travel.add_flight(Flight('02', '', 4))
+    assert travel.get_num_clients() == 12
