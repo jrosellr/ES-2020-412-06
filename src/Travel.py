a1c60cd69390011416a7b8e6cee8a7da328dd88c
joan.rosellr <joan.rosellr@gmail.com>
2020-05-16 17:47:34 +0200

    add failing Reservation ctor unit test

--- src/Travel.py
+++ src/Travel.py
@@ -1,6 +1,9 @@
+from src.Flights import Flights
+
+
 class Travel:
 
-    def __init__(self, flights: Flights, hotels: Hotels, cars: Cars):
+    def __init__(self, flights: Flights, hotels=None, cars=None):
         self.flights = flights
         self.hotels = hotels
         self.cars = cars
---
+++ tests/test_reservation.py
@@ -0,0 +1,24 @@
+from src.Reservation import Reservation
+from src.Travel import Travel
+from src.User import User
+from src.Flights import Flights
+from src.Flight import Flight
+from src.PaymentData import PaymentData
+
+
+def test_reservation_ctor():
+    """ Unit test for Reservation.__init__(**)
+
+    :return: None
+    """
+
+    usr = User('Test', '000000', 'test/address', '666777888', 'test@example.com')
+    travel = Travel(Flights([
+        Flight('00', 'test', 0)
+    ]))
+    payment = PaymentData('Test', '000000', '000', 5)
+    reservation = Reservation(travel, usr, payment)
+    travel.flights.modify_flight('00', new_destination='Berlin')
+    assert isinstance(reservation, Reservation)
+    assert reservation.travel == travel
+    assert reservation.travel.flights.flights['00'] != travel.flights.flights['00']
