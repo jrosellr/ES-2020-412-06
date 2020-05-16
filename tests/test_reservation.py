df5d9a7352622af6b8af0392173b0ebbe3a57d7a
joan.rosellr <joan.rosellr@gmail.com>
2020-05-16 23:00:18 +0200

    add unit test to Reservation, tests price calculation after the deletion of all flights

--- tests/test_reservation.py
+++ tests/test_reservation.py
@@ -163,3 +163,22 @@ def test_reservation_calculate_flights_price_delete_flight():
     assert reservation.calculate_flights_price(5) == 20  # 2 Flights * 2 Clients per Flight * 5 = 20
     reservation.delete_flight('00')
     assert reservation.calculate_flights_price(5) == 10  # 1 Flight * 2 Clients per Flight * 5 = 10
+
+
+def test_reservation_calculate_flights_price_delete_all_flights():
+    """ Unit test for Reservation.calculate_flights_price(**)
+
+        Calculated price should be number of clients * price of flight per client * number of flights
+        :return: None
+    """
+
+    usr = User('Test', '000000', 'test/address', '666777888', 'test@example.com')
+    travel = Travel(Flights([
+        Flight('00', 'Berlin', 2),
+        Flight('01', 'Roma', 2)
+    ]))
+    reservation = Reservation(travel, usr)
+    assert reservation.calculate_flights_price(5) == 20  # 2 Flights * 2 Clients per Flight * 5 = 20
+    reservation.delete_flight('00')
+    reservation.delete_flight('01')
+    assert reservation.calculate_flights_price(5) == 0  # 0 Flight * 0 Clients per Flight * 5 = 0
