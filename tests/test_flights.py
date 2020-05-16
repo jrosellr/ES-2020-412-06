f0469d07534350e80e0d6f7aedd67a49df0b3c69
joan.rosellr <joan.rosellr@gmail.com>
2020-05-16 13:27:48 +0200

    refactor Flights class unit tests

--- tests/test_flights.py
+++ tests/test_flights.py
@@ -2,7 +2,13 @@ from src.Flight import Flight
 from src.Flights import Flights
 
 
-def test_init():
+def test_flights_ctor():
+    """ Test case for flight.__init__(**) method
+
+        ---
+
+        :return: None
+    """
     f1 = Flight('00', 'Berlin', 4)
     f2 = Flight('01', 'Berlin', 4)
     f3 = Flight('03', 'Berlin', 4)
@@ -19,16 +25,12 @@ def test_init():
     assert len(flights.flights) == 1
 
 
-def test_modify_flight():
-    """ Test suite for modify_flight(**) method
+def test_modify_flight_destination():
+    """ Test case for modify_flight(**) method
 
-    ---
-    Tests three main usage cases:
-    *   We want to modify one attribute only
-    *   We want to modify many attributes at the same time
-    *   We want to modify a non-existing flight
+        ---
 
-    :return: None
+        :return: None
     """
 
     f1 = Flight('00', 'Berlin', 4)
@@ -40,16 +42,57 @@ def test_modify_flight():
     assert flights.flights['00'].destination == 'Roma'
     assert flights.flights['00'].num_clients == 4
 
+
+def test_modify_flight_num_clients():
+    """ Test case for modify_flight(**) method
+
+        ---
+
+        :return: None
+    """
+
+    f1 = Flight('00', 'Berlin', 4)
+    f2 = Flight('01', 'Berlin', 4)
+    f3 = Flight('03', 'Berlin', 4)
+    flights = Flights([f1, f2, f3])
+
     flights.modify_flight('00', new_num_clients=2)
     assert flights.flights['00'].num_clients != 4
     assert flights.flights['00'].num_clients == 2
-    assert flights.flights['00'].destination == 'Roma'
+    assert flights.flights['00'].destination == 'Berlin'
+
+
+def test_modify_flight_all_params():
+    """ Test case for modify_flight(**) method
+
+        ---
+
+        :return: None
+    """
+
+    f1 = Flight('00', 'Berlin', 4)
+    f2 = Flight('01', 'Berlin', 4)
+    f3 = Flight('03', 'Berlin', 4)
+    flights = Flights([f1, f2, f3])
 
     flights.modify_flight('00', new_destination='Stockholm', new_num_clients=5)
-    assert flights.flights['00'].destination != 'Roma'
+    assert flights.flights['00'].destination != 'Berlin'
     assert flights.flights['00'].destination == 'Stockholm'
-    assert flights.flights['00'].num_clients != 2
+    assert flights.flights['00'].num_clients != 4
     assert flights.flights['00'].num_clients == 5
 
-    flights.modify_flight('05', new_destination='Stockholm', new_num_clients=5)
+
+def test_modify_flight_invalid_code():
+    """ Test case for modify_flight(**) method
+
+        ---
+
+        :return: None
+    """
+    f1 = Flight('00', 'Berlin', 4)
+    f2 = Flight('01', 'Berlin', 4)
+    f3 = Flight('03', 'Berlin', 4)
+    flights = Flights([f1, f2, f3])
+
+    flights.modify_flight('04', new_destination='Stockholm', new_num_clients=5)
     assert '05' not in flights.flights
