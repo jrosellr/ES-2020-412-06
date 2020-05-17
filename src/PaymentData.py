d31661967c286fbe75a26475315a03e185a9a1c0
joan.rosellr <joan.rosellr@gmail.com>
2020-05-17 16:46:12 +0200

    add documentation to PaymentData ctor

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -13,10 +13,17 @@ class PaymentData:
             amount: The amount of money to be charged
 
             credit_card_type: The card can be either VISA or MASTERCARD
-
     """
 
     def __init__(self, user_name: str, card_number: str, security_code: str, amount: float):
+        """ Construct a new PaymentData object
+
+        :param user_name: The user's name
+        :param card_number: The card's number
+        :param security_code: The card's CVV
+        :param amount: The amount of money to be charged
+        """
+
         self.user_name = user_name
         self.card_number = card_number
         self.security_code = security_code
