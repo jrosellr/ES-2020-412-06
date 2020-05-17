a6bbcf50a8a48db4f07e2af2a31a02ff0d54ec7f
joan.rosellr <joan.rosellr@gmail.com>
2020-05-17 16:43:33 +0200

    improve PaymentData documentation

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -1,11 +1,19 @@
 class PaymentData:
     """ A Value-Object used to hold the payment data
 
-    :var user_name: The client's name
-    :var card_number: The card identifier number
-    :var security-code: Three-number security code
-    :var amount: The amount to be charged
-    :var credit_card_type: The card can be either VISA or MASTERCARD
+        ----
+
+        Instance variables:
+            user_name: The client's name
+
+            card_number: The card number
+
+            security-code: CVV (Card Validation Value, 3-digit number for VISA and MASTERCARD)
+
+            amount: The amount of money to be charged
+
+            credit_card_type: The card can be either VISA or MASTERCARD
+
     """
 
     def __init__(self, user_name: str, card_number: str, security_code: str, amount: float):
