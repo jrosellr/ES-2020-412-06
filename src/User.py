998ccfa45fbbd54c4ff83795ca4e5de8984c0044
joan.rosellr <joan.rosellr@gmail.com>
2020-05-17 16:37:46 +0200

    add documentation to User ctor

--- src/User.py
+++ src/User.py
@@ -3,6 +3,15 @@ class User:
 
     """
     def __init__(self, full_name: str, nif: str, address: str, mobile_number: str, email: str):
+        """ Construct a new User object
+
+        :param full_name: The first name and last name of the user
+        :param nif: The identification code of the user (DNI)
+        :param address: The user's address
+        :param mobile_number: The user's mobile phone number
+        :param email: The user's email address
+        """
+
         self.full_name = full_name
         self.nif = nif
         self.address = address
