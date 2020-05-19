7477da9e4c290ef53732d75a5e936fca3c92b0f6
joan.rosellr <joan.rosellr@gmail.com>
2020-05-19 20:18:27 +0200

    fix typo

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ def mock_bank_error(monkeypatch):
 
 @pytest.fixture
 def mock_bank_success(monkeypatch):
-    def mock_do_payment_succesful(*args):
+    def mock_do_payment_successful(*args):
         return MOCKED_BANK_SUCCESS_RET
 
-    monkeypatch.setattr(Bank, "do_payment", mock_do_payment_succesful)
+    monkeypatch.setattr(Bank, "do_payment", mock_do_payment_successful)
