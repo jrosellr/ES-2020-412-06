46ec3775066e17c0c71e44bb3bd8b5e39a661f4c
joan.rosellr <joan.rosellr@gmail.com>
2020-05-20 20:18:24 +0200

    add documentation to Flights and Flight unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from src.Skyscanner import Skyscanner
 from src.Reservation import Reservation
 from src.Bank import Bank
+from src.Flight import Flight
+from src.Flights import Flights
 from .test_constants import *
 
 # TODO: add documentation to fixtures
@@ -50,3 +52,20 @@ def mock_confirm_reserve_return_retries(monkeypatch, mock_skyscanner_error):
         return retries
 
     monkeypatch.setattr(Reservation, "_confirm_flights", mock_confirm_reserve)
+
+
+@pytest.fixture
+def default_flight_list():
+    f0 = Flight(DEFAULT_FLIGHT_CODE_0, DEFAULT_FLIGHT_DESTINATION, DEFAULT_FLIGHT_PASSENGERS)
+    f1 = Flight(DEFAULT_FLIGHT_CODE_1, DEFAULT_FLIGHT_DESTINATION, DEFAULT_FLIGHT_PASSENGERS)
+    f2 = Flight(DEFAULT_FLIGHT_CODE_2, DEFAULT_FLIGHT_DESTINATION, DEFAULT_FLIGHT_PASSENGERS)
+    return [f0, f1, f2]
+
+
+@pytest.fixture
+def default_flights(default_flight_list):
+    return Flights(default_flight_list)
+
+@pytest.fixture
+def default_new_flight():
+    return Flight(DEFAULT_NEW_FLIGHT_CODE, DEFAULT_FLIGHT_DESTINATION, DEFAULT_FLIGHT_PASSENGERS)
--- tests/test_flight.py
+++ tests/test_flight.py
@@ -1,7 +1,7 @@
 from src.Flight import Flight
+from .test_constants import *
 
-
-def test_flight_ctor():
+def test_flight_ctor(default_flight_list):
     """ Test case for flight.__init__(**) method
 
         ---
@@ -9,7 +9,7 @@ def test_flight_ctor():
         :return: None
     """
 
-    f = Flight('', '', 0)
-    assert f.passengers == 0
-    assert f.code == ''
-    assert f.destination == ''
+    default_flight_0 = default_flight_list[0]
+    assert default_flight_0.code == DEFAULT_FLIGHT_CODE_0
+    assert default_flight_0.passengers == DEFAULT_FLIGHT_PASSENGERS
+    assert default_flight_0.destination == DEFAULT_FLIGHT_DESTINATION
