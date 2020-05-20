44a2c326f52d4666c872e600830ea9c62c54c5fd
Arnau Closa <arnauclosam@gmail.com>
2020-05-20 18:35:23 +0200

    add "test_retries_confirm_flights"

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from src.Skyscanner import Skyscanner
+from src.Reservation import Reservation
 from src.Bank import Bank
 from .test_constants import *
 
@@ -27,3 +28,25 @@ def mock_bank_success(monkeypatch):
         return MOCKED_BANK_SUCCESS_RET
 
     monkeypatch.setattr(Bank, "do_payment", mock_do_payment_successful)
+
+
+@pytest.fixture
+def mock_skyscanner_error(monkeypatch):
+    def mock_confirm_reserve_error(*args):
+        raise ConnectionRefusedError("Connection with SkyScanner failed.")
+
+    monkeypatch.setattr(Skyscanner, "confirm_reserve", mock_confirm_reserve_error)
+
+
+@pytest.fixture
+def mock_confirm_reserve_return_retries(monkeypatch, mock_skyscanner_error):
+    def mock_confirm_reserve(*args):
+        retries = 0
+        while retries < 3:
+            try:
+                return Skyscanner.confirm_reserve(*args)
+            except ConnectionRefusedError:
+                retries += 1
+        return retries
+
+    monkeypatch.setattr(Reservation, "_confirm_flights", mock_confirm_reserve)
