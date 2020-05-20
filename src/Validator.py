fb6df8c1c7aa01a925b2dbf90cc092c6f2543b7b
lucasgp99 <lucasesp99@gmail.com>
2020-05-20 18:26:09 +0200

    create class "Validator"

---
+++ src/Validator.py
@@ -0,0 +1,19 @@
+from src.Reservation import Reservation
+
+class Validator:
+
+    def __init__(self,_email_regexp: str,_credit_card_number_regexp: str,_credit_card_sec_code_regexp: str):
+        self._email_regexp = None
+        self._credit_card_number_regexp = None
+        self._credit_card_sec_code_regexp = None
+
+    def validate_email(self,email: str):
+
+    def validate_credit_card_number(self, card_number: str):
+
+    def validate_credit_code(self, card_sec_code: str):
+
+
+
+
+
