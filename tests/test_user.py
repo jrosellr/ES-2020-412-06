a7d2f283a8a78993007d10965e785ceb455b310c
joan.rosellr <joan.rosellr@gmail.com>
2020-05-21 12:02:33 +0200

    fix failing test

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,7 +11,7 @@ def test_user_ctor():
     
     user = User(DEFAULT_USER_NAME, DEFAULT_DNI, DEFAULT_ADDRESS, DEFAULT_MOBILE_NUMBER, DEFAULT_USER_EMAIL)
     assert user.full_name == DEFAULT_USER_NAME
-    assert user.nif == DEFAULT_DNI
+    assert user.dni == DEFAULT_DNI
     assert user.address == DEFAULT_ADDRESS
     assert user.mobile_number == DEFAULT_MOBILE_NUMBER
     assert user.email == DEFAULT_USER_EMAIL
