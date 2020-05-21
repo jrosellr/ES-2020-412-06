214d467562256dfdcff4f06faff61e76c964f362
lucasgp99 <lucasesp99@gmail.com>
2020-05-21 16:20:33 +0200

    add class Hotel + test_hotel.py

--- src/Hotels.py
+++ src/Hotels.py
@@ -1,11 +1,37 @@
-class Hotels:  # FIXME: This is supposed to be a Hotel aggregation
-    """ A Value-Object used to hold the Hotel(s) an user wants to reserve
+from src.Hotel import Hotel
 
+
+class Hotels:
+    """ A Value-Object that holds the different Hotels a user wants to reserve
+
+        ----
+
+            Instance variables:
+                Hotels: uses a dictionary to enforce the singularity of each hotel
     """
 
-    def __init__(self, code, name, num_clients, num_rooms, days_reserved):
-        self.code = code
-        self.name = name
-        self.num_clients = num_clients
-        self.num_rooms = num_rooms
-        self.days_reserved = days_reserved
+    def __init__(self, hotels: list):
+        """ Creates a dictionary using the hotel codes as keys
+
+        :param hotels: a list of objects of type Hotel
+        """
+        if len(hotels) != 0:
+            self._hotels = {hotel.code: hotel for hotel in hotels}
+        else:
+            self._hotels: dict = {}
+
+    def __getitem__(self, key: str):
+        if key in self._hotels:
+            return self._hotels[key]
+
+    def __setitem__(self, key: str, value: Hotel):
+        if key not in self._hotels:
+            self._hotels[key] = value
+
+    def __delitem__(self, key: str):
+        if key in self._hotels:
+            del self._hotels[key]
+
+    def __len__(self):
+        return len(self._hotels)
+
