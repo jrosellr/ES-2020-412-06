74fd63c9bc07f2b379aeab3d4f6a854d413481d0
Arnau Closa <arnauclosam@gmail.com>
2020-05-21 16:47:05 +0200

    add car class & add cars methods

--- src/Cars.py
+++ src/Cars.py
@@ -1,9 +1,20 @@
+from src.Flight import Flight
+
+
 class Cars:
     """ A Value-Object used to hold the car data
 
     """
-    def __init__(self, code, brand, pick_up_place, days_reserved):  # FIXME: This is supposed to be a Car aggregate
-        self.code = code
-        self.brand = brand
-        self.pick_up_place = pick_up_place
-        self.days_reserved = days_reserved
+    def __init__(self, cars: list):
+
+        if len(cars) != 0:
+            self._cars = {car.code: car for car in cars}
+        else:
+            self._cars: dict = {}
+
+    def __getitem__(self, key: str):
+        if key in self._cars:
+            return self._cars[key]
+
+    def __len__(self):
+        return len(self._cars)
