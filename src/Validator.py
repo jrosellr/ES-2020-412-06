48de635fcaecf1faac8c5d598acf02eb693714dc
Arnau Closa <arnauclosam@gmail.com>
2020-05-21 11:54:25 +0200

    fix test_validator + validator class

--- src/Validator.py
+++ src/Validator.py
@@ -5,40 +5,43 @@ class Validator:
     """ Handles the validation of regular expressions of paymentData
 
     """
-    # FIXME: If error in _validate functions, return an exception, not a bool
+    _name_regexp = r'[A-Za-z ]{1,30}'
+    _dni_regexp = r'\d{8}[A-Z]'
+    _email_regexp = r'\w{1,30}@\w{2,20}\.\w{2,5}'
+    _credit_card_number_regexp = r'\d{4} \d{4} \d{4} \d{4}'
+    _credit_card_cvv_regexp = r'\d\d\d'
 
-    def __init__(self, _email_regexp: str, _credit_card_number_regexp: str, _credit_card_sec_code_regexp: str):
-        # FIXME: Variables not used in the class
-        self._email_regexp = None
-        self._credit_card_number_regexp = None
-        self._credit_card_sec_code_regexp = None
-
-    def _validate_full_name(name: str):
-        if re.fullmatch('[A-Za-z ]{1,30}', name) is not None:
+    @staticmethod
+    def validate_full_name(name: str):
+        if re.fullmatch(Validator._name_regexp, name) is not None:
             return True
         else:
             return False
 
-    def _validate_dni(dni: str):
-        if re.fullmatch('\d{8}[A-Z]', dni) is not None:
+    @staticmethod
+    def validate_dni(dni: str):
+        if re.fullmatch(Validator._dni_regexp, dni) is not None:
             return True
         else:
             return False
 
-    def _validate_email(email: str):
-        if re.fullmatch('\w{1,30}@\w{2,20}\.\w{2,5}', email) is not None:
+    @staticmethod
+    def validate_email(email: str):
+        if re.fullmatch(Validator._email_regexp, email) is not None:
             return True
         else:
             return False
 
-    def _validate_credit_card_number(card_number: str):
-        if re.fullmatch('\d{4} \d{4} \d{4} \d{4}', card_number) is not None:
+    @staticmethod
+    def validate_credit_card_number(card_number: str):
+        if re.fullmatch(Validator._credit_card_number_regexp, card_number) is not None:
             return True
         else:
             return False
 
-    def _validate_credit_security_code(security_code: str):
-        if re.fullmatch('\d\d\d', security_code) is not None:
+    @staticmethod
+    def validate_credit_security_code(security_code: str):
+        if re.fullmatch(Validator._credit_card_cvv_regexp, security_code) is not None:
             return True
         else:
             return False
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -15,16 +15,17 @@ def test_validate_payment_info():
     card_number = '1234 3456 5678 7890'
     security_code = '123'
 
-    assert Validator._validate_full_name(name) == True
-    assert Validator._validate_full_name(name) != False
-    assert Validator._validate_dni(dni) == True
-    assert Validator._validate_dni(dni) != False
-    assert Validator._validate_email(email) == True
-    assert Validator._validate_email(email) != False
-    assert Validator._validate_credit_card_number(card_number) == True
-    assert Validator._validate_credit_card_number(card_number) != False
-    assert Validator._validate_credit_security_code(security_code) == True
-    assert Validator._validate_credit_security_code(security_code) != False
+    assert Validator.validate_full_name(name) is not None
+    assert Validator.validate_full_name(name) is True
+    assert Validator.validate_dni(dni) is not None
+    assert Validator.validate_dni(dni) is True
+    assert Validator.validate_email(email) is not None
+    assert Validator.validate_email(email) is True
+    assert Validator.validate_credit_card_number(card_number) is not None
+    assert Validator.validate_credit_card_number(card_number) is True
+    assert Validator.validate_credit_security_code(security_code) is not None
+    assert Validator.validate_credit_security_code(security_code) is True
+
 
 def test_validate_payment_info_error():
     """ Test check invalid payment info
@@ -42,13 +43,13 @@ def test_validate_payment_info_error():
     card_number = '4568 98761 234 5698'
     security_code = 'a56'
 
-    assert Validator._validate_full_name(name) != True
-    assert Validator._validate_full_name(name) == False
-    assert Validator._validate_dni(dni) != True
-    assert Validator._validate_dni(dni) == False
-    assert Validator._validate_email(email) != True
-    assert Validator._validate_email(email) == False
-    assert Validator._validate_credit_card_number(card_number) != True
-    assert Validator._validate_credit_card_number(card_number) == False
-    assert Validator._validate_credit_security_code(security_code) != True
-    assert Validator._validate_credit_security_code(security_code) == False
+    assert Validator.validate_full_name(name) is not None
+    assert Validator.validate_full_name(name) is False
+    assert Validator.validate_dni(dni) is not None
+    assert Validator.validate_dni(dni) is False
+    assert Validator.validate_email(email) is not None
+    assert Validator.validate_email(email) is False
+    assert Validator.validate_credit_card_number(card_number) is not None
+    assert Validator.validate_credit_card_number(card_number) is False
+    assert Validator.validate_credit_security_code(security_code) is not None
+    assert Validator.validate_credit_security_code(security_code) is False
