0355f79a558338d5e4c2688b87f28338f6e726b3
DavidMRi <davmenrib@gmail.com>
2020-05-23 12:10:41 +0200

    improve documentation of user.py

--- src/User.py
+++ src/User.py
@@ -1,6 +1,17 @@
 class User:
     """ A Value-Object used to hold the user data
+        ----
 
+        Instance variables:
+        full_name: The first name and last name of the user
+
+        dni: The identification code of the user (DNI)
+
+        address: The user's address
+
+        mobile_number: The user's mobile phone number
+
+        email: The user's email address
     """
 
     def __init__(self, full_name: str, dni: str, address: str, mobile_number: str, email: str):
